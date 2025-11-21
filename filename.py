ed71dce852d186d8d4dc06cbd75d94453e568106
Kavyatotad1406 <totadkavya@gmail.com>
2025-11-21 10:35:59 +0530

    filename.py

--- filename.py
+++ filename.py
@@ -1,14 +1,15 @@
 import sys
 
-# If user provides argument, use it; else use default filename
+# Jenkins does not pass arguments unless configured
+# So first check if we *actually* received one
 if len(sys.argv) > 1:
     file = sys.argv[1]
-    print("User provided filename:", file)
+    print("User provided filename:")
 else:
-    print("No input given - Default value is used:")
+    print("No input given - Default value is:")
     file = "cartoon.img"
 
-# Check file extension
+# Checking extension
 if file.endswith(".img"):
     print("It's an image")
 elif file.endswith(".mp4"):
